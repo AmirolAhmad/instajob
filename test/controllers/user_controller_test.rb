445e36401b264442668f8dfd02dc09e096879e7e
AmirolAhmad <9w2pdf@gmail.com>
2013-09-03 05:49:48 +0800

    Test helper require
    More info: https://github.com/plataformatec/devise#test-helpers

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
+sign_in :user, @user   # sign_in(scope, resource)
+sign_in @user          # sign_in(resource)
+
+sign_out :user         # sign_out(scope)
+sign_out @user         # sign_out(resource)
+
 class UserControllerTest < ActionController::TestCase
   test "should get show" do
   	get :show
