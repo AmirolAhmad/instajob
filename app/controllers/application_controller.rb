e80fbf8cfb262956ce5b8a5f28e92babcae7a079
AmirolAhmad <9w2pdf@gmail.com>
2013-09-02 19:58:44 +0800

    redirect user to login page after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  	#redirect user to login page after sign out
+  	def after_sign_out_path_for(resource)
+	  session_path(resource_name)
+	end
 end
