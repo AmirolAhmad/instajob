85bb7194579150667e16815b1d02132f93218988
AmirolAhmad <9w2pdf@gmail.com>
2013-09-03 11:07:51 +0800

    redirect user to profile page after profile update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  	#redirect user to login page after sign out
-  	def after_sign_out_path_for(resource)
-	  session_path(resource_name)
+  #redirect user to login page after sign out
+  def after_sign_out_path_for(resource)
+	  login_path
 	end
 
 	#redirect user to profile page after sign in
 	def after_sign_in_path_for(resource)
 	  profile_path
 	end
+
+  #redirect user to profile page after profile update
+  def after_update_path_for(resource)
+    accounts_settings_path
+  end
 
 	# permit user to login with username or email
 	before_filter :configure_permitted_parameters, if: :devise_controller?
