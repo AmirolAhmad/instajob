ed510db9f19509e61af9bc37d3dc5833d95c1b30
AmirolAhmad <9w2pdf@gmail.com>
2013-09-05 17:02:14 +0800

    fixed config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Instajob::Application.routes.draw do
+  devise_for :members
   devise_scope :member do
     get '/logout' => 'devise/sessions#destroy'
     get '/login', :to => 'devise/sessions#new'
--- db/migrate/20130905080344_add_full_name_to_members.rb
+++ db/migrate/20130905080344_add_full_name_to_members.rb
@@ -2,5 +2,7 @@ class AddFullNameToMembers < ActiveRecord::Migration
   def change
     add_column :members, :first_name, :string
     add_column :members, :last_name, :string
+    add_index :members, :first_name
+    add_index :members, :last_name
   end
 end
