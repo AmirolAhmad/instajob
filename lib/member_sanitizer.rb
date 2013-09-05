919622b630627ae5cb98f817709ab0dbd9caa6b7
AmirolAhmad <9w2pdf@gmail.com>
2013-09-05 19:32:31 +0800

    update login, signup & update with sanitizer (strong parameter)

--- lib/member_sanitizer.rb
+++ lib/member_sanitizer.rb
@@ -3,4 +3,12 @@ class Member::ParameterSanitizer < Devise::ParameterSanitizer
     def account_update
         default_params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password)
     end
+
+    def sign_up
+    	default_params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end
+
+    def sign_in
+    	default_params.permit(:username, :email, :password, :remember_me)
+    end
 end
