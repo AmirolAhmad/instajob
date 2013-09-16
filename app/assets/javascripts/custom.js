dc843c45ffac87040cd1f6d6a19909efc2cd9133
AmirolAhmad <9w2pdf@gmail.com>
2013-09-16 13:59:19 +0800

    Minify custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,79 +1,19 @@
 //tab for job category
 $(function () {
-    $('#featured a[href="#permanent"]').tab('show')
-});
-
-//tooltip for add to wishlist
-$(function () {
-	$('[rel=wishlist]').tooltip()
-});
-
-//tooltip for view all details
-$(function () {
-	$('[rel=details]').tooltip()
-});
-
-//tooltip for show-all job
-$(function () {
-	$('[rel=show-all]').tooltip()
-});
-
-//tooltip for back to top
-$(function () {
-	$('[rel=to-top]').tooltip()
-});
-
-//tooltip for account setting
-$(function () {
-	$('[rel=acc-sett]').tooltip()
-});
-
-//tooltip for logout
-$(function () {
-	$('[rel=logout]').tooltip()
-});
-
-//tooltip for notification
-$(function () {
-	$('[rel=notification]').tooltip()
-});
-
-//tooltip for subscription
-$(function () {
-	$('[rel=subscription]').tooltip()
-});
-
-//tooltip for dashboard
-$(function () {
-	$('[rel=dashboard]').tooltip()
-});
-
-//tooltip for account type
-$(function () {
-	$('[rel=acc-type]').tooltip()
-});
-
-//tooltip for star user
-$(function () {
-	$('[rel=star-user]').tooltip()
-});
-
-//tooltip for verify user
-$(function () {
-	$('[rel=verify]').tooltip()
-});
-
-//tooltip for edit profile
-$(function () {
-	$('[rel=edit]').tooltip()
-});
-
-//tooltip for post a job
-$(function () {
-	$('[rel=post-job]').tooltip()
-});
-
-//tooltip to view all job
-$(function () {
-	$('[rel=view-all]').tooltip()
+    $('#featured a[href="#permanent"]').tab('show');
+    $('[rel=wishlist]').tooltip();
+	$('[rel=details]').tooltip();
+	$('[rel=show-all]').tooltip();
+	$('[rel=to-top]').tooltip();
+	$('[rel=acc-sett]').tooltip();
+	$('[rel=logout]').tooltip();
+	$('[rel=notification]').tooltip();
+	$('[rel=subscription]').tooltip();
+	$('[rel=dashboard]').tooltip();
+	$('[rel=acc-type]').tooltip();
+	$('[rel=star-user]').tooltip();
+	$('[rel=verify]').tooltip();
+	$('[rel=edit]').tooltip();
+	$('[rel=post-job]').tooltip();
+	$('[rel=view-all]').tooltip();
 });
